c01a0581dfbf2baf254105a5d4aeee72e350ae2e
Vansh Zaveri <vanshzaveri93@gmail.com>
2017-05-11 22:22:00 -0700

    Models

--- src/main/java/edu/sjsu/cmpe275/model/Company.java
+++ src/main/java/edu/sjsu/cmpe275/model/Company.java
@@ -1,5 +1,116 @@
 package edu.sjsu.cmpe275.model;
 
-public class Company {
+import java.util.ArrayList;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+@Table(name="Company")
+public class Company {
+	
+	
+	/*Company_id
+	Email
+	Name
+	Website
+	Logo image url
+	Address_of_headquarter
+	Description
+Verification_code
+Is_verified*/
+	
+	@Id 
+	@Column(name="COMPANY_ID")
+	@GeneratedValue(generator="system-uuid") 
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
+	private String companyId;
+	
+	@Column(name="EMAIL", nullable = false)
+	private String email;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="WEBSITE")
+	private String website;
+	
+	@Column(name="IMAGE_URL")
+	private String imageURL;
+	
+	@Column(name="ADDRESS")
+	private String address;
+	
+	@Column(name="DESCRIPTION")
+	private String description;
+	
+	@Column(name="VERIFICATION_CODE")
+	private String verificationCode;
+	
+	@Column(name="IS_VERIFIED")
+	private boolean isVerified;
+	
+	
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getVerificationCode() {
+		return verificationCode;
+	}
+	public void setVerificationCode(String verificationCode) {
+		this.verificationCode = verificationCode;
+	}
+	public boolean getIsVerified() {
+		return isVerified;
+	}
+	public void setIsVerified(boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+	
+	
 }
--- src/main/java/edu/sjsu/cmpe275/model/Education.java
+++ src/main/java/edu/sjsu/cmpe275/model/Education.java
@@ -1,5 +1,56 @@
 package edu.sjsu.cmpe275.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Education {
 
+/*	School
+	Degree
+	field_of_study	
+		gpa*/
+	
+	
+	@Column(name="SCHOOL")
+	private String school;
+	
+	@Column(name="DEGREE")
+	private String degree;
+	
+	@Column(name="FIELD_OF_STUDY")
+	private String fieldOfStudy;
+	
+	@Column(name="GPA")
+	private String gpa;
+	
+	
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getFieldOfStudy() {
+		return fieldOfStudy;
+	}
+	public void setFieldOfStudy(String fieldOfStudy) {
+		this.fieldOfStudy = fieldOfStudy;
+	}
+	public String getGpa() {
+		return gpa;
+	}
+	public void setGpa(String gpa) {
+		this.gpa = gpa;
+	}
+	
+	
+	
+	
 }
